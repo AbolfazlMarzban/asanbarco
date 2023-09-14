45d96d7e4e22f1824e898ed977356c1b9332e6e6
Abolfazl <abolfazlmarzban39@gmail.com>
2023-09-14 11:13:39 +0430

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/** @type {import('next').NextConfig} */ 
+const withPWA = require("next-pwa");
+
+const nextConfig = withPWA({
+    dest: "public",
+    register: true,
+    skipWaiting: true
+})
+
 
 module.exports = nextConfig
