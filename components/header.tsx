5774390dcdb04fd34e9941aa59745c039ab810c7
Abolfazl <abolfazlmarzban39@gmail.com>
2023-09-15 17:53:06 +0430

    abol1402-6-24

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,82 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
 
 function Header() {
-    return (
-        <div>
-            
-        </div>
-    );
+    const menu  = [
+        {
+            name: 'بار های من',
+            link: 'myCargo'
+        },
+        {
+            name: 'بار های در حال حمل',
+            link: 'myCargo'
+        },
+        {
+            name: 'بار های همه روزه',
+            link: 'myCargo'
+        },
+        {
+            name: 'آماروارقام بارها',
+            link: 'myCargo'
+        },
+        {
+            name: 'سوابق حمل',
+            link: 'myCargo'
+        },
+    ]
+  return (
+    <div>
+        <div className="w-full h-14 bg-myblue p-4 text-center">
+            <label className="text-white">آسان بار</label>
+        <button className="float-left mr-3 text-white " >
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+            >
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+            />
+            </svg>
+        </button>
+        <Link href={"/"} className="float-left mr-2 text-white">
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+            >
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+            />
+            </svg>
+        </Link>
+        </div> 
+        
+<div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+    <ul className="flex flex-wrap -mb-px">
+        {menu.map((item, i) => (
+
+        <li className="mr-2" key={i}>
+            <a href={'/cargolist/'+item.link} className="inline-block p-4 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" aria-current="page">
+                {item.name}</a>
+        </li>
+        ))}
+       
+    </ul>
+</div>
+
+    </div>
+  );
 }
 
 export default Header;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar() {
   return (
     <div className="footer fixed bottom-0 w-full  border-[1px] border-opacity-100 flex flex-row gap-3 justify-around text-color space-aroun z-1 bg-white py-2">
-      <button className="fill-[#0000ff] text-[#0000ff]">
+      <button className="fill-[#0000ff] text-[#0000ff] flex flex-col items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -21,7 +21,7 @@ function Navbar() {
 
         <span className="text-xs">مدیریت</span>
       </button>
-      <button>
+      <button className="flex flex-col items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -44,27 +44,40 @@ function Navbar() {
 
         <span className="text-xs">آسان</span>
       </button>
-      <button>
+      <button className="flex flex-col items-center">
         <svg
-          className="m-auto w-[25px] h-[25px]"
           xmlns="http://www.w3.org/2000/svg"
-          height="1em"
-          viewBox="0 0 512 512"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
         >
-          <path d="M190.5 68.8L225.3 128H224 152c-22.1 0-40-17.9-40-40s17.9-40 40-40h2.2c14.9 0 28.8 7.9 36.3 20.8zM64 88c0 14.4 3.5 28 9.6 40H32c-17.7 0-32 14.3-32 32v64c0 17.7 14.3 32 32 32H480c17.7 0 32-14.3 32-32V160c0-17.7-14.3-32-32-32H438.4c6.1-12 9.6-25.6 9.6-40c0-48.6-39.4-88-88-88h-2.2c-31.9 0-61.5 16.9-77.7 44.4L256 85.5l-24.1-41C215.7 16.9 186.1 0 154.2 0H152C103.4 0 64 39.4 64 88zm336 0c0 22.1-17.9 40-40 40H288h-1.3l34.8-59.2C329.1 55.9 342.9 48 357.8 48H360c22.1 0 40 17.9 40 40zM32 288V464c0 26.5 21.5 48 48 48H224V288H32zM288 512H432c26.5 0 48-21.5 48-48V288H288V512z" />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M16.5 18.75h-9m9 0a3 3 0 013 3h-15a3 3 0 013-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 01-.982-3.172M9.497 14.25a7.454 7.454 0 00.981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 007.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 002.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 012.916.52 6.003 6.003 0 01-5.395 4.972m0 0a6.726 6.726 0 01-2.749 1.35m0 0a6.772 6.772 0 01-3.044 0"
+          />
         </svg>
+
         <span className="text-xs">باشگاه</span>
       </button>
-      <button>
+      <button className="flex flex-col items-center">
         <svg
-          className="m-auto  w-[25px] h-[25px]"
           xmlns="http://www.w3.org/2000/svg"
-          height="1em"
-          viewBox="0 0 448 512"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
         >
-          {/* !Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc */}
-          <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+          />
         </svg>
+
         <span className="text-xs">پروفایل</span>
       </button>
     </div>
