6decf6e1e3073ae9db2227409fab983b0c3b8dda
Abolfazl <abolfazlmarzban39@gmail.com>
2023-09-15 22:21:43 +0430

    abol 1402-6-25

--- pages/cargolist/index.tsx
+++ pages/cargolist/index.tsx
@@ -4,10 +4,13 @@ import React from 'react';
 
 function CargoList() {
     return (
-        <>
+        <div className='w-full h-screen'>
             <Header></Header>
+            <div className='w-full min-h-full flex flex-col items-center justify-center'>
+                <img src="/empty-bg.png" alt="" className='w-24' />
+            </div>
             <Navbar></Navbar>
-        </>
+        </div>
     );
 }
 
