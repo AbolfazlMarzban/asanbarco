ca723892b477a628bee5fb4144d5de8e8baf7a69
Abolfazl <abolfazlmarzban39@gmail.com>
2023-09-16 16:28:21 +0430

    abol 1402-6-25

--- components/select.tsx
+++ components/select.tsx
@@ -1,30 +1,15 @@
 import React, { useState } from "react";
 
-function LocationSelect({ data, title, getLocation }: any) {
+function Select({ data, title, getLocation,children, placeholder }: any) {
   return (
-    <div className="bg-white border shadow flex items-center p-3 rounded-md mt-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        className="w-6 h-6 text-myblue"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-        />
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-        />
-      </svg>
-      <label htmlFor="">{title}</label>
+    <div className="bg-white border shadow flex items-center justify-between p-3 rounded-md mt-2">
+      <div className="flex ">
+
+      {children}
+      <label htmlFor="" className="mr-1">{title}</label>
+      </div>
       <select name="location" id="location" className="mx-3">
-        <option value="">شهر واستان موردنظرتان را انتخاب کنید</option>
+        <option value="">{placeholder}</option>
         {data.map((item: any, i: any) => (
           <option key={i} value={item}>
             {item}
@@ -53,4 +38,4 @@ function LocationSelect({ data, title, getLocation }: any) {
   );
 }
 
-export default LocationSelect;
+export default Select;
