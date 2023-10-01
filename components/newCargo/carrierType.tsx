5e34cdc8efc43706d4d62706d54ed1e263df6973
Abolfazl <abolfazlmarzban39@gmail.com>
2023-10-01 07:49:04 +0330

    abol 1402-7-8

--- components/newCargo/carrierType.tsx
+++ components/newCargo/carrierType.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
-function CarrierType({icon, name, active}:any) {
+function CarrierType({icon, name, active, id , selectItem}:any) {
+    function select(){
+        selectItem(id)
+    }
     return (
-        <div className='flex flex-col bg-white p-2 rounded-lg justify-center items-center mx-2'>
+        <div className='flex flex-col bg-white p-2 rounded-xl justify-center items-center mx-2' onClick={()=>select()}>
             {/* {icon} */}
-            <img src={icon} alt="" />
+            <img src={icon} alt="" className='max-w-sm w-32'/>
             <label htmlFor="" className='text-sm mt-2'>{name}</label>
+            {active && (
             <p className='mt-2 text-sm'>
                 <span className='ml-1'>{active}</span>
                 ناوگان فعال
             </p>
+
+            )}
         </div>
     );
 }
--- components/newCargo/setCarrier.tsx
+++ components/newCargo/setCarrier.tsx
@@ -1,38 +1,93 @@
 import React from "react";
 import Dialog from "../UI/dialog";
 import CarrierType from "./carrierType";
+import { useState } from "react";
 
 function SetCarrier({ title, close }: any) {
+const [carrierType, setCarrierType] = useState(null)
+const [carrierChildren, setcarrierChildren] = useState([])
+function selectCarrierType(id:any){
+  var cari:any = carriers.find(item => item.id == id)
+  setCarrierType(cari)
+}
 var carriers = [
     {
         icon: '/traily.png',
         name: 'تریلی',
-        active: 120
+        active: 120,
+        id: 0,
+        children: [
+          "کفی",
+          "بغلدار",
+          "کانتینردار",
+          "کمپرسی",
+          "ترانزیت",
+          "یخچال دار",
+          "تیغه دار(کانتینر بر)",
+          "تانکر مخزن دار",
+        ],
     },
     {
         icon: '/tak.png',
         name: 'تک',
-        active: 82
+        active: 82,
+        id: 1,
+        children: ["اطاقدار", "کمپرسی", "مسقف", "یخچال دار", "تانکر مخزن دار"]
     },
     {
         icon: '/joft.png',
         name: 'جفت',
-        active: 49
+        active: 49,
+        id: 2,
+        children: [
+          "اطاق دار",
+          "کمپرسی",
+          "مسقف",
+          "تانکر مخزن دار",
+          "یخچال دار",
+        ]
     },
     {
         icon: '/khavar.png',
         name: 'خاور و کامیونت',
-        active: 396
+        active: 396,
+        id: 3,
+        children: [
+          "اطاقدار",
+          "کمپرسی",
+          "مسقف",
+          "یخچال دار",
+          "تانکر مخزن دار",
+          "911",
+        ]
     },
     {
         icon: '/neisan.png',
         name: 'وانت و نیسان',
-        active: 325
+        active: 325,
+        id: 4,
+        children: [
+          "پراید وانت",
+          "پیکان وانت ",
+          "نیسان",
+          "آریسان",
+          "نیسان یخچالی",
+        ]
     },
     {
         icon: '/others.png',
         name: 'سایر ناوگان ها',
-        active: null
+        active: null,
+        id: 5,
+        children : [
+          "تانکر مخزن دار",
+          "بوژی",
+          "کمرشکن",
+          "بونکر",
+          "جرثقیل دار",
+          "جانبو",
+          "میکسر",
+        ]
     }
 ]
   function closeDialog() {
@@ -41,7 +96,7 @@ var carriers = [
   return (
     <>
       <Dialog title={title} close={() => closeDialog()}>
-        <div className="w-full h-full bg-slate-300 p-6">
+        <div className="w-full h-full bg-slate-300 p-6 overflow-auto">
           <div className="w-full border border-red-300 flex items-center gap-2 p-2 bg-white rounded-lg text-red-300">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -62,17 +117,35 @@ var carriers = [
               کیلومتری مبدأ بار شما می باشند.
             </p>
           </div>
-          <div className="flex p-2">
+          <div className="flex flex-wrap items-stretch p-2">
             {carriers.map((item, i)=> (
-            <div className="basis-1/3" key={i}>
+            <div className="basis-1/3 my-2 cursor-pointer" key={i}>
             <CarrierType
                 icon={item.icon}
                 name={item.name}
                 active={item.active}
+                id={item.id}
+                selectItem={(id:any)=>selectCarrierType(id)}
             ></CarrierType>
             </div>
-
             ))}
+          </div>
+          <hr className="w-full" />
+          <div>
+             {carrierType?.children.map((child:any, i:any) => (
+              <div className="flex p-2 bg-white my-2 rounded-lg justify-between items-center" key={i}>
+                <div>
+                <input type="checkbox" name="" id="" className="ml-2" />
+                <label htmlFor="">{child}</label>
+                </div>
+                <span className="text-xs text-slate-400">21 ناوگان فعال</span>
+              </div>
+             ))}
+          </div>
+          <div className="flex justify-center items-center mt-5">
+            <button className="w-full px-4 py-2 bg-green-300 text-white rounded-lg">
+              ثبت و ادامه
+            </button>
           </div>
         </div>
       </Dialog>
