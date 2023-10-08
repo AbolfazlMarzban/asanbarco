fe16ba260195149feed4290ce14629cdaacc4131
Abolfazl <abolfazlmarzban39@gmail.com>
2023-10-08 23:01:19 +0330

    abol 1402-7-16

--- components/cargoList/cargoBox.tsx
+++ components/cargoList/cargoBox.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
 
 function CargoBox() {
     return (
-        <div className='w-full mx-1 my-2 p-2 bg-white border rounded-lg flex'>
-            <div className='basis-1/2 flex flex-col items-center'>
+        <div className='flex flex-col w-full mx-1 my-2 p-2 bg-white border rounded-lg'>
+        <div className='flex pb-4'>
+            <div className='basis-1/2 flex flex-col items-center border-l-2'>
                 <label htmlFor="">تهران</label>
                 <span>تهران</span>
             </div>
             <div className='basis-1/2 flex flex-col items-center'>
             <label htmlFor="">تهران</label>
                 <span>تهران</span>
+            </div>
+        </div>
+            <div className='flex border-t-2 justify-between'>
+                <div className='basis-1/2 flex'>
+                    <LocalShippingOutlinedIcon className='border border-myblue-2'></LocalShippingOutlinedIcon>
+                </div>
             </div>
         </div>
 
