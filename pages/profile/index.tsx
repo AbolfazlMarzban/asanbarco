826f8350f9293bdcf6b65351e6b6b10aa71f8fad
Abolfazl <abolfazlmarzban39@gmail.com>
2023-10-11 00:21:09 +0330

    abol 1402-7-18

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -5,7 +5,13 @@ import Navbar from "@/components/navbar";
 function index() {
   return (
     <div className="w-full h-screen">
-        
+      <div className="h-full bg-myblue">
+            <div className="flex flex-col items-center pt-6">
+                <div className="w-16 h-16 rounded-full border-2 ">
+
+                </div>
+            </div>
+      </div>
       <Navbar></Navbar>
     </div>
   );
