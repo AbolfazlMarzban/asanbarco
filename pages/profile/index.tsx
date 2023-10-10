0877dc70028919e524df202c371dfb292ca4ba93
Abolfazl <abolfazlmarzban39@gmail.com>
2023-10-11 00:10:17 +0330

    abol 1402-7-18

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -5,14 +5,7 @@ import Navbar from "@/components/navbar";
 function index() {
   return (
     <div className="w-full h-screen">
-      <Header></Header>
-      <div className="w-full min-h-full flex flex-col items-center justify-center">
-        <img src="/empty-bg.png" alt="" className="w-36" />
-        <p className="text-zinc-400	mt-4 w-72 text-center">
-          همین حالا سفارش خودتان را در آسان بار ثبت کنید و از مزایای آن بهره مند
-          شوید
-        </p>
-      </div>
+        
       <Navbar></Navbar>
     </div>
   );
