cea5e770e31b8ae6ad8eeed7bab41548f3221e9c
Abolfazl <abolfazlmarzban39@gmail.com>
2023-10-15 00:18:23 +0330

    abol 1402-7-22

--- pages/club/scoresHistory/index.tsx
+++ pages/club/scoresHistory/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 function index() {
-    return (
-        <div className='w-full h-screen'>
-                  <div className="bg-myblue text-white py-5 px-2 flex justify-between">
+  return (
+    <div className="w-full h-screen bg-mybg">
+      <div className="bg-myblue text-white py-5 px-2 flex justify-between">
         <div className="flex gap-4">
           <Link href={"/club"}>
             <svg
@@ -25,8 +25,19 @@ function index() {
           <h2>تاریخچه امتیازات و جوایز</h2>
         </div>
       </div>
+      <div className="w-full flex flex-col p-3 ">
+        <div className="border border-red-600 bg-white rounded-lg flex justify-between items-center p-3">
+            <div className="flex items-center gap-3">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z" />
+</svg>
+ <span>معرفی آسان بار به دوستان</span>
+            </div>
+            <span className="text-red-600">10 امتیاز</span>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default index;
