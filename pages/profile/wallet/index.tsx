dfc3f5949fbed6b39d7a5187d8bc977af82390c0
Abolfazl <abolfazlmarzban39@gmail.com>
2023-10-14 07:28:22 +0330

    abol 1402-7-22

--- pages/profile/wallet/index.tsx
+++ pages/profile/wallet/index.tsx
@@ -6,8 +6,17 @@ import { useState } from "react";
 
 function index() {
   const [display, setDisplay] = useState("pay");
-  const [suggested, setSuggested] = useState('')
-  const moneyList = ['5,000', '10,000', '20,000', '30,000', '40,000', '50,000', '100,000', '200,000']
+  const [suggested, setSuggested] = useState("");
+  const moneyList = [
+    "5,000",
+    "10,000",
+    "20,000",
+    "30,000",
+    "40,000",
+    "50,000",
+    "100,000",
+    "200,000",
+  ];
   return (
     <div className="w-full h-screen">
       <div className="bg-myblue text-white py-5 px-2 flex justify-between">
@@ -53,33 +62,74 @@ function index() {
           <button
             onClick={() => setDisplay("history")}
             className={`${
-              display == "history"
-                ? `bg-white `
-                : ``
+              display == "history" ? `bg-white ` : ``
             } text-myblue p-2 w-1/2 rounded-md`}
           >
             لیست تراکنش ها
           </button>
         </div>
-        {display == 'pay' ? (
+        {display == "pay" ? (
           <div className="flex flex-col items-start w-full">
-              <label htmlFor="">مبالغ پیشنهادی:</label>
-              <div className="flex flex-wrap w-full justify-center">
-                {moneyList.map(item => (
-                <div className="basis-1/4  p-2" onClick={()=>setSuggested(item)}>
-                  <div className={`${suggested == item ? `bg-cyan-400 text-white` : ``} flex flex-col items-center cursor-pointer border border-cyan-400 rounded-xl py-2`}>
+            <label htmlFor="">مبالغ پیشنهادی:</label>
+            <div className="flex flex-wrap w-full justify-center">
+              {moneyList.map((item) => (
+                <div
+                  className="basis-1/4  p-2"
+                  onClick={() => setSuggested(item)}
+                >
+                  <div
+                    className={`${
+                      suggested == item ? `bg-cyan-400 text-white` : ``
+                    } flex flex-col items-center cursor-pointer border border-cyan-400 rounded-xl py-2`}
+                  >
                     <span className="text-sm">{item}</span>
                     <span className="text-sm">تومان</span>
                   </div>
                 </div>
-                ))}
+              ))}
+            </div>
+            <label htmlFor="">مبالغ دلخواه:</label>
+            <div className="flex items-stretch w-full gap-2 mt-2">
+              <div className="w-1/4 bg-green-200 text-green-600 text-center flex justify-center items-center p-2 rounded-xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 6v12m6-6H6"
+                  />
+                </svg>
               </div>
+              <div className="w-1/2">
+                <input type="number" className="w-full border-2 border-cyan-300 rounded-xl text-center h-full" />
+              </div>
+              <div className="w-1/4 bg-red-200 text-red-600 text-center flex justify-center items-center p-2 rounded-xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M18 12H6"
+                  />
+                </svg>
+              </div>
+            </div>
           </div>
-        ) :
-          (
-            <></>
-          )
-        }
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
