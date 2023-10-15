aee2b762b7fcd39ef72d2780b8315acb2a4d34b1
Abolfazl <abolfazlmarzban39@gmail.com>
2023-10-16 02:07:32 +0330

    abol 1402-7-23

---
+++ pages/profile/support/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Link from "next/link";
+
+function index() {
+  return (
+    <div className="w-full h-screen bg-mybg">
+      <div className="bg-myblue text-white py-5 px-2 flex justify-between w-full">
+        <div className="flex gap-4">
+          <Link href={"/profile"}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+              />
+            </svg>
+          </Link>
+          <h2>پشتیبانی آسان بار</h2>
+        </div>
+      </div>
+      <div className="flex flex-col h-3/4 mt-3 p-4">
+            <span>لیست مشکلات شما:</span>
+      </div>
+    </div>
+  );
+}
+
+export default index;
