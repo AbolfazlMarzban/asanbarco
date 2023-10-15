bcee54ffcbf65adf8c2906ee3fa3982bd1121d0a
Abolfazl <abolfazlmarzban39@gmail.com>
2023-10-16 00:25:25 +0330

    abol 1402-7-23

--- pages/club/scoresGuide/index.tsx
+++ pages/club/scoresGuide/index.tsx
@@ -1,32 +1,99 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+import Accardion from "@/components/UI/accardion";
 
 function index() {
-    return (
-        <div className='w-full h-screen'>
-        <div className="bg-myblue text-white py-5 px-2 flex justify-between">
-<div className="flex gap-4">
-<Link href={"/club"}>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke-width="1.5"
-    stroke="currentColor"
-    className="w-6 h-6"
-  >
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-    />
-  </svg>
-</Link>
-<h2>راهنمای امتیازات</h2>
-</div>
-</div>
-</div>
-    );
+  return (
+    <div className="w-full h-screen bg-mybg">
+      <div className="bg-myblue text-white py-5 px-2 flex justify-between">
+        <div className="flex gap-4">
+          <Link href={"/club"}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+              />
+            </svg>
+          </Link>
+          <h2>راهنمای امتیازات</h2>
+        </div>
+      </div>
+      <div className="w-full flex flex-col p-3 ">
+        <div className="border border-red-600 bg-white rounded-lg flex justify-between items-center p-3">
+          <div className="flex items-center gap-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z"
+              />
+            </svg>
+            <span>معرفی آسان بار به دوستان</span>
+          </div>
+          <span className="text-red-600">10 امتیاز</span>
+        </div>
+        <div className="w-full flex gap-3 p-3 items-center">
+          <div className="w-1/2 flex flex-col justify-center items-center border border-green-600 rounded-lg p-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span className="text-green-600">15 امتیاز</span>
+            <span>تأیید درخواست رانندگان</span>
+          </div>
+          <div className="w-1/2 flex flex-col justify-center items-center border border-myblue rounded-lg p-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+              />
+            </svg>
+            <span className="text-myblue">10 امتیاز</span>
+            <span>خوش آمدگویی</span>
+          </div>
+        </div>
+        <div className="flex flex-col items-start mt-3">
+            <label htmlFor="">سوالات متداول:</label>
+            <Accardion
+            title={'چگونه مبدأ و مقصد بارگیری را تعیین کنم؟'} 
+            ></Accardion>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default index;
--- pages/club/scoresHistory/index.tsx
+++ pages/club/scoresHistory/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Link from "next/link";
-import Accardion from "@/components/UI/accardion";
-
+import { useState } from "react";
 function index() {
+  const [display, setDisplay] = useState("score")
   return (
     <div className="w-full h-screen bg-mybg">
-      <div className="bg-myblue text-white py-5 px-2 flex justify-between">
+      <div className="bg-myblue text-white py-5 px-2 flex justify-between w-full">
         <div className="flex gap-4">
           <Link href={"/club"}>
             <svg
@@ -26,72 +26,48 @@ function index() {
           <h2>تاریخچه امتیازات و جوایز</h2>
         </div>
       </div>
-      <div className="w-full flex flex-col p-3 ">
-        <div className="border border-red-600 bg-white rounded-lg flex justify-between items-center p-3">
-          <div className="flex items-center gap-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z"
-              />
-            </svg>
-            <span>معرفی آسان بار به دوستان</span>
-          </div>
-          <span className="text-red-600">10 امتیاز</span>
+      <div className="bg-white rounded-xl flex gap-5 p-2 mt-3 mx-2">
+          <button
+            onClick={() => setDisplay("score")}
+            className={`${
+              display == "score" ? `bg-myblue text-white` : `bg-white text-myblue`
+            } p-2 w-1/2 rounded-md`}
+          >
+            تاریخچه امتیازات
+          </button>
+          <button
+            onClick={() => setDisplay("bonus")}
+            className={`${
+              display == "bonus" ? `bg-myblue text-white` : `bg-white text-myblue`
+            } p-2 w-1/2 rounded-md`}
+          >
+            تاریخچه جوایز
+          </button>
         </div>
-        <div className="w-full flex gap-3 p-3 items-center">
-          <div className="w-1/2 flex flex-col justify-center items-center border border-green-600 rounded-lg p-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <span className="text-green-600">15 امتیاز</span>
-            <span>تأیید درخواست رانندگان</span>
-          </div>
-          <div className="w-1/2 flex flex-col justify-center items-center border border-myblue rounded-lg p-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
-              />
-            </svg>
-            <span className="text-myblue">10 امتیاز</span>
-            <span>خوش آمدگویی</span>
-          </div>
+        <div className="bg-white h-full m-3 rounded-lg p-3">
+          {display == 'score' 
+            ?
+            (
+              <>    
+                <div className="flex justify-between items-center">
+                  <div className="flex flex-col justify-start">
+                    <span className="">خوش آمدگویی</span>
+                    <span className="text-myblue mt-3">10 امتیاز</span>
+                  </div>
+                  <div className="flex flex-col items-end">
+                    <span className="text-myblue">افزایش</span>
+                    <span className="mt-3">1402/07/23 16:33</span>
+                  </div>
+                </div>
+                <hr className="mt-2"></hr>
+              </>
+            )
+            :
+            (
+              <></>
+            )
+          }
         </div>
-        <div className="flex flex-col items-start mt-3">
-            <label htmlFor="">سوالات متداول:</label>
-            <Accardion
-            title={'چگونه مبدأ و مقصد بارگیری را تعیین کنم؟'} 
-            ></Accardion>
-        </div>
-      </div>
     </div>
   );
 }
