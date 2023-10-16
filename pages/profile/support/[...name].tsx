13406ce0b1e3009e2d26dba18949ad7261e1209b
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-10-16 10:44:06 +0330

    abol 1402-7-23

--- pages/profile/support/[...name].tsx
+++ pages/profile/support/[...name].tsx
@@ -7,7 +7,7 @@ function index() {
     <div className="w-full h-screen bg-mybg">
       <div className="bg-myblue text-white py-5 px-2 flex justify-between w-full">
         <div className="flex gap-4">
-          <Link href={"/profile"}>
+          <Link href={"/profile/support"}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -23,12 +23,11 @@ function index() {
               />
             </svg>
           </Link>
-          <h2>پشتیبانی آسان بار</h2>
+          <h2>بار ویژه</h2>
         </div>
       </div>
       <div className="flex flex-col h-3/4 mt-3 p-4">
         <span>لیست مشکلات شما:</span>
-        <Link href={'/profile/support/بارویژه'}>        
         <DialogBtn
           title={"مشاهده سوالات مربوط به پکیج بار ویژه"}
           secondIcon={
@@ -48,7 +47,6 @@ function index() {
             </svg>
           }
         ></DialogBtn>
-        </Link>
         <DialogBtn
           title={"کرایه و نحوه پرداخت"}
           secondIcon={
@@ -88,24 +86,6 @@ function index() {
           }
         ></DialogBtn>
         <hr className="my-3" />
-        <div className="bg-white flex items-center p-3 rounded-lg justify-between">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
-            />
-          </svg>
-          <span>چت با پشتیبانی</span>
-          <span className="text-slate-400">(آفلاین)</span>
-        </div>
       <button className="bg-green-300 text-white flex items-center p-3 mt-10 mx-2 rounded-lg justify-center">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
