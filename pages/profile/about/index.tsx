e8a7061a10f6a13a5f03061abaa3d78ebe495550
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-10-16 12:32:40 +0330

    abol 1402-7-24

---
+++ pages/profile/about/index.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import TelegramIcon from '@mui/icons-material/Telegram';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LanguageIcon from '@mui/icons-material/Language';
+
+function index() {
+  return (
+    <div className="w-full h-screen bg-mybg">
+      <div className="bg-myblue text-white py-5 px-2 flex justify-between w-full">
+        <div className="flex gap-4">
+          <Link href={"/profile"}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+              />
+            </svg>
+          </Link>
+          <h2>درباره آسان بار</h2>
+        </div>
+      </div>
+      <div className="m-3 flex flex-col p-3 items-center">
+        <img src="/logo.png" className="w-64"></img>
+        <h3 className="text-myblue text-xl font-bold">آسان بار</h3>
+        <span className="mt-3 text-slate-300">نسخه 1.0.0</span>
+        <div className="flex items-center justify-center gap-4 mt-3">
+            <TelegramIcon fontSize="large"></TelegramIcon>
+            <InstagramIcon fontSize="large"></InstagramIcon>
+            <LanguageIcon fontSize="large"></LanguageIcon>
+        </div>
+        <p className="max-w-full w-1/2 mt-3">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+      </div>
+    </div>
+  );
+}
+
+export default index;
