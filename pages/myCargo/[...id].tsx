d846a534135b285c1fef950461a606a418b35b2d
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-10-21 12:24:39 +0330

    abol 1402-7-29

--- pages/myCargo/[...id].tsx
+++ pages/myCargo/[...id].tsx
@@ -215,80 +215,7 @@ function index() {
               </div>
             </Dialog>
           )}
-        </>
-      ) : (
-        <>
-          <div className=" mx-4 bg-white border-[1px] rounded-md  ">
-            <div className="small_details grid grid-cols-2 justify-items-stretch ">
-              <div className="border-b-[1px] border-l-[1px] border-slate-100  px-2 py-3">
-                <h3 className="text-gray-400 text-sm">نام راننده</h3>
-                علی مرادی
-              </div>
-              <div className="border-b-[1px]  border-slate-100 px-2 py-3 ">
-                <h3 className="text-gray-400 text-sm pb-2">شماره راننده</h3>
-                0918341441
-              </div>
-              <div className="border-b-[1px] border-l-[1px] border-slate-100 px-2 py-3 ">
-                <div
-                  className="flex items-center gap-4 cursor-pointer"
-                  onClick={() => setConfirm(true)}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    className="w-10 h-10 border-2 border-myblue p-1 rounded-full text-myblue"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M4.5 12.75l6 6 9-13.5"
-                    />
-                  </svg>
-
-                  <span className="text-myblue">تأیید راننده</span>
-                </div>
-              </div>
-              <div className="border-b-[1px]  border-slate-100  px-2 py-3">
-                <div className="flex items-center gap-4">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    className="w-10 h-10 border-2 border-myblue p-1 rounded-full text-myblue"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
-                    />
-                  </svg>
-
-                  <span className="text-myblue">تماس با راننده</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          {confirm && (
-            <Dialog title={"تأیید راننده"} close={() => setConfirm(false)}>
-              <div className="flex flex-col px-3 py-2">
-                <span>آیا این بار را حمل خواهد کرد؟</span>
-                <div className="flex gap-3 mx-3 my-2">
-                  <button className="bg-slate-300 rounded-mdc w-1/2">
-                    خیر
-                  </button>
-                  <button className="bg-myblue text-white rounded-md w-1/2">
-                    بله
-                  </button>
-                </div>
-              </div>
-            </Dialog>
-          )}
-          {showEdit && (
+           {showEdit && (
             <Dialog title={"ویرایش بار"} close={() => setShowEdit(false)}>
               <>
                 <div className="flex flex-col items-center bg-[#f1f5f8] justify-start p-2">
@@ -601,28 +528,28 @@ function index() {
                       ></Textbox>
                     </Accordion>
                     <div className="w-full flex justify-center align-center bg-[#f1f5f8] p-4">
-                      {/* <Link href={'/newCargo/specialOffer'}> 
-            <button className="flex gap-2 px-3 py-2 bg-orange-400 text-white rounded-xl mx-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-                />
-              </svg>
-              <span>ثبت بار ویژه</span>
-            </button>
-            </Link>
-            <button className="flex gap-2 px-3 py-2 bg-teal-400 text-white rounded-xl mx-2" onClick={()=>registerCargo()}>
-              <span>ثبت بار عادی</span>
-            </button> */}
+                                  {/* <Link href={'/newCargo/specialOffer'}> 
+                        <button className="flex gap-2 px-3 py-2 bg-orange-400 text-white rounded-xl mx-2">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                            />
+                          </svg>
+                          <span>ثبت بار ویژه</span>
+                        </button>
+                        </Link>
+                        <button className="flex gap-2 px-3 py-2 bg-teal-400 text-white rounded-xl mx-2" onClick={()=>registerCargo()}>
+                          <span>ثبت بار عادی</span>
+                        </button> */}
                     </div>
                   </div>
                 </div>
@@ -661,6 +588,80 @@ function index() {
               </>
             </Dialog>
           )}
+        </>
+      ) : (
+        <>
+          <div className=" mx-4 bg-white border-[1px] rounded-md  ">
+            <div className="small_details grid grid-cols-2 justify-items-stretch ">
+              <div className="border-b-[1px] border-l-[1px] border-slate-100  px-2 py-3">
+                <h3 className="text-gray-400 text-sm">نام راننده</h3>
+                علی مرادی
+              </div>
+              <div className="border-b-[1px]  border-slate-100 px-2 py-3 ">
+                <h3 className="text-gray-400 text-sm pb-2">شماره راننده</h3>
+                0918341441
+              </div>
+              <div className="border-b-[1px] border-l-[1px] border-slate-100 px-2 py-3 ">
+                <div
+                  className="flex items-center gap-4 cursor-pointer"
+                  onClick={() => setConfirm(true)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-10 h-10 border-2 border-myblue p-1 rounded-full text-myblue"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+
+                  <span className="text-myblue">تأیید راننده</span>
+                </div>
+              </div>
+              <div className="border-b-[1px]  border-slate-100  px-2 py-3">
+                <div className="flex items-center gap-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-10 h-10 border-2 border-myblue p-1 rounded-full text-myblue"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+                    />
+                  </svg>
+
+                  <span className="text-myblue">تماس با راننده</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {confirm && (
+            <Dialog title={"تأیید راننده"} close={() => setConfirm(false)}>
+              <div className="flex flex-col px-3 py-2">
+                <span>آیا این بار را حمل خواهد کرد؟</span>
+                <div className="flex gap-3 mx-3 my-2">
+                  <button className="bg-slate-300 rounded-mdc w-1/2">
+                    خیر
+                  </button>
+                  <button className="bg-myblue text-white rounded-md w-1/2">
+                    بله
+                  </button>
+                </div>
+              </div>
+            </Dialog>
+          )}
+         
         </>
       )}
     </div>
