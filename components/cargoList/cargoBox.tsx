0adc7a90101716e5b19746eaa60526d3afeed2da
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-10-21 10:13:00 +0330

    abol 1402-7-29

--- components/cargoList/cargoBox.tsx
+++ components/cargoList/cargoBox.tsx
@@ -5,12 +5,12 @@ function CargoBox({data}:any) {
     <div className="flex flex-col w-full mx-1 my-2 p-2 bg-white border rounded-lg relative">
       <div className="flex pb-4">
         <div className="basis-1/2 flex flex-col items-center border-l-2 py-3">
-          <label htmlFor="">{data.desination.split(',')[1]}</label>
-          <span>{data.desination.split(',')[0]}</span>
+          <label htmlFor="">{data.origin.split(',')[1]}</label>
+          <span>{data.origin.split(',')[0]}</span>
         </div>
         <div className="basis-1/2 flex flex-col items-center py-3">
-          <label htmlFor="">تهران</label>
-          <span>تهران</span>
+          <label htmlFor="">{data.desination.split(',')[1]}</label>
+          <span>{data.desination.split(',')[0]}</span>
         </div>
       </div>
       <div className="flex border-t-2 justify-between pt-2">
@@ -29,7 +29,9 @@ function CargoBox({data}:any) {
               d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
             />
           </svg>
-          <span>وانت و نیسان، پراید وانت</span>
+          {data.carrier.map((item:any, i:any) => (            
+          <span key={i}>{item.parent}, {item.child}</span>
+          ))}
         </div>
         <div className="basis-1/2 flex items-center gap-4">
           <div className="text-myblue border-2 border-myblue w-10 h-10 p-0 text-center rounded-full flex items-center justify-center">
