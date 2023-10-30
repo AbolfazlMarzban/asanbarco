0e75d66875792cebcdff5d337e62f165f8725d35
Abolfazl <abolfazlmarzban39@gmail.com>
2023-10-30 19:35:01 +0330

    abol 1402-8-8

--- pages/api/paymentManage.ts
+++ pages/api/paymentManage.ts
@@ -6,6 +6,6 @@ export default async function handler(req: any, res: any) {
   if(method == "POST"){
     const userID = req.body.userID
     const payAmount = req.body.payAmount
-    
+   var code = '942ab142-ed88-4412-8c9f-ab8658074bcf' 
   }
 }
