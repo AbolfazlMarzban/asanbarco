39ade731ed4429a9b594bf42dd2d22a9bcc30130
Abolfazl <abolfazlmarzban39@gmail.com>
2023-11-28 21:02:09 +0330

    abol 1402-9-7

--- components/cargoList/cargoBox.tsx
+++ components/cargoList/cargoBox.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 function CargoBox({data}:any) {
+  function isSpecial(){
+    console.log('data', data)
+    return true
+  }
   return (
     <div className="flex flex-col w-full mx-1 my-2 p-2 bg-white border rounded-lg relative">
       <div className="flex pb-4">
@@ -56,6 +60,24 @@ function CargoBox({data}:any) {
           />
         </svg>
       </div>
+      {isSpecial() && (
+      <div className="flex items-center p-2 gap-2 text-yellow-400 font-bold">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+              clip-rule="evenodd"
+            />
+          </svg>
+
+          <span>ویژه</span>
+        </div>
+      )}
     </div>
   );
 }
