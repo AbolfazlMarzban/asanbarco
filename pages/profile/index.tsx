0704fe6e6fa7be0a6a2de03ee3ec6725c1316f04
Abolfazl <abolfazlmarzban39@gmail.com>
2023-12-28 22:08:32 +0330

    abol 1402-10-7

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -338,7 +338,7 @@ function index() {
                   viewBox="0 0 24 24"
                   strokeWidth="1.5"
                   stroke="currentColor"
-                  className="w-6 h-6"
+                  className="w-4 h-4"
                 >
                   <path
                     stroke-linecap="round"
