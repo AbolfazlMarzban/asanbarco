92e1738e87c33c2a2a33c764620e8f19adff9a9e
Abolfazl <abolfazlmarzban39@gmail.com>
2023-12-29 19:39:18 +0330

    abol 1402-10-8

--- pages/profile/about/index.tsx
+++ pages/profile/about/index.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import TelegramIcon from '@mui/icons-material/Telegram';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LanguageIcon from '@mui/icons-material/Language';
+import TelegramIcon from "@mui/icons-material/Telegram";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LanguageIcon from "@mui/icons-material/Language";
 
 function index() {
   return (
@@ -27,16 +27,27 @@ function index() {
           <h2>درباره آسان بار</h2>
         </div>
       </div>
-      <div className="m-3 flex flex-col p-3 items-center">
+      <div className="flex flex-col p-3 pb-6 items-center bg-mybg">
         <img src="/logo.png" className="w-64"></img>
         <h3 className="text-myblue text-xl font-bold">آسان بار</h3>
         <span className="mt-3 text-slate-300">نسخه 1.0.0</span>
         <div className="flex items-center justify-center gap-4 mt-3">
-            <TelegramIcon fontSize="large"></TelegramIcon>
-            <InstagramIcon fontSize="large"></InstagramIcon>
-            <LanguageIcon fontSize="large"></LanguageIcon>
+          <TelegramIcon fontSize="large"></TelegramIcon>
+          <InstagramIcon fontSize="large"></InstagramIcon>
+          <LanguageIcon fontSize="large"></LanguageIcon>
         </div>
-        <p className="max-w-full w-1/2 mt-4">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+        <p className="max-w-1/2 w-full mt-4 px-3">
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+          از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
+          سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای
+          متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه
+          درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با
+          نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان
+          خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید
+          داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به
+          پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی
+          سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+        </p>
       </div>
     </div>
   );
